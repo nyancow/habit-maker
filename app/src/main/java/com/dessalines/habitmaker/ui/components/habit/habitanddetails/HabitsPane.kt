f29548ddd95c96a6efeae4f8b1d4e7071aa3abec
Dessalines <dessalines@users.noreply.github.com>
2025-01-01 13:47:23 -0500

    Add fastlane generation (#12)
    * Removing pointless import.
    
    * Adding fastlane generation.

--- app/src/main/java/com/dessalines/habitmaker/ui/components/habit/habitanddetails/HabitsPane.kt
+++ app/src/main/java/com/dessalines/habitmaker/ui/components/habit/habitanddetails/HabitsPane.kt
@@ -21,7 +21,6 @@ import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material.icons.outlined.Close
-import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
